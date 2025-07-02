959d5ba9080937e91203d03f63a9742857fa2ba3
MRamazan <mramazantrn28@gmail.com>
2025-07-02 06:43:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -292,8 +292,9 @@ def get_languages():
     })
 
 def main():
-    port = int(os.environ.get("PORT", 5000))  # Railway burada 'PORT' verir
-    app.run(debug=False, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host="0.0.0.0", port=port)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
