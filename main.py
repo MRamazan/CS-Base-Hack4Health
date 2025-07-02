4fc51146f653e60496f3aaa8bce2876e036c5cb4
MRamazan <mramazantrn28@gmail.com>
2025-07-02 06:48:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -292,8 +292,7 @@ def get_languages():
     })
 
 def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 if __name__ == "__main__":
     main()
